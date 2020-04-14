b9811f8d0e026d24fdff60a93a584bce289143f3
Ulysse Gaspar <ulysse.gaspar@free.fr>
2020-04-14 08:47:40 +0200

    fix apples, tweaked dark mode

--- snake_numworks.py
+++ snake_numworks.py
@@ -22,46 +22,48 @@ if mRapide == "n":
         t = int(t)
 else:
     vitesse, cMode, t = 8, "white", 10
-L, l = 320//t, 200//t
+L, l = 320 // t, 200 // t
 
 noir, blanc, rouge, vert = (0, 0, 0), (255, 255, 255), (255, 0, 0), (0, 255, 0)
-vers = [[l//2, n] for n in range(L//2-2, L//2+2)]
-pomme = False
+vers = [[l // 2, n] for n in range(L // 2 - 2, L // 2 + 2)]
+px, py = vers[0][0], vers[0][1]
 dx, dy = 0, 1
-px, py = 10, 14
 score = 0
 
-draw_string("Score: 0", 5, 203)
 if cMode == "white":
     fill_rect(0, 200, 320, 1, noir)
 else:
-    fill_rect(0, 0, 320, 200, noir)
+    fill_rect(0, 0, 320, 222, noir)
+    fill_rect(0, 200, 320, 1, blanc)
 
 while True:
-    if not pomme:
-        while [px, py] in vers:
-            px, py = randint(0, l-1), randint(0, L-1)
-        fill_rect(py*t, px*t, t, t, rouge)
-        pomme = True
+    if cMode == "white":
+            draw_string("Score: "+str(score), 5, 203)
+        else:
+            draw_string("Score: "+str(score), 5, 203, blanc, noir)
+
+    while [px, py] in vers:
+        px, py = randint(0, l - 1), randint(0, L - 1)
+    fill_rect(py * t, px * t, t, t, rouge)
+
     t1 = monotonic()
-    while monotonic()-t1 < 1/vitesse:
+    while monotonic() - t1 < 1 / vitesse:
         for dir in [[0, 0, -1], [1, -1, 0], [2, 1, 0], [3, 0, 1]]:
             if keydown(dir[0]):
                 dx, dy = dir[1], dir[2]
+
     first, last = vers[0], vers[-1]
     nx, ny = last[0] + dx, last[1] + dy
     if 0 <= nx < l and 0 <= ny < L and [nx, ny] not in vers:
         vers.append([nx, ny])
-        fill_rect(ny*t, nx*t, t, t, vert)
+        fill_rect(ny * t, nx * t, t, t, vert)
     else:
         break
     if [nx, ny] == [px, py]:
-        pomme = False
         score += 1
-        draw_string(str(score), 75, 203)
     else:
         del vers[0]
         if cMode == "white":
-            fill_rect(first[1]*t, first[0]*t, t, t, blanc)
+            fill_rect(first[1] * t, first[0] * t, t, t, blanc)
         else:
-            fill_rect(first[1]*t, first[0]*t, t, t, noir)
+            fill_rect(first[1] * t, first[0] * t, t, t, noir)
